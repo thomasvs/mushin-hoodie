56a8d19e5890a7225089d52e45d02da10307a0de
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2014-08-06 12:35:16 -0400

    fix added space by converting tabbar to A

--- app/scripts/directives/tabbar.js
+++ app/scripts/directives/tabbar.js
@@ -3,7 +3,10 @@ angular.module('zentodone')
   .directive('tabBar', function () {
 
     return {
-      restrict: 'E',
+      // FIXME: as an E, it adds whitespace right above the tasklist;
+      // as an A, it seems to sort-of-work, but I had to remove the
+      // bp-tabbar
+      restrict: 'EA',
       scope: {
           active: '@active',
       },

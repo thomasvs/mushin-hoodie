0ccaeebbfcf8c6214db3823559beefc065fc8cc7
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2014-08-09 20:55:03 -0400

    clean up indentation

--- app/scripts/controllers/task.js
+++ app/scripts/controllers/task.js
@@ -1,11 +1,13 @@
-angular.module('zentodone')
-  .controller('TaskCtrl', function (tasks, Task, $scope, $state, hoodie, $rootScope) {
-    var lastType
-    var params = $state.params
-    var current = $state.current
+// vi:si:et:sw=2:sts=2:ts=2
+angular.module('zentodone').controller(
+  'TaskCtrl',
+  function (tasks, Task, $scope, $state, hoodie, $rootScope) {
+    var lastType;
+    var params = $state.params;
+    var current = $state.current;
     var debug = new window.$debug('zentodone:controllers/TaskCtrl');
 
-    $scope.task = {}
+    $scope.task = {};
 
     $scope.contexts = $rootScope.contexts;
     $scope.projects = $rootScope.projects;
@@ -32,59 +34,59 @@ angular.module('zentodone')
     });
 
     // FIXME: hack: if the page was reloaded, contexts and projects are
-    //              not loaded.  force a load.
-    //              only works with ECMA5
+    //        not loaded.  force a load.
+    //        only works with ECMA5
     if (Object.keys($scope.contexts).length === 0) {
-        debug('controller/task.js: getting all tasks');
-        tasks.getAll(Task.INBOX);
+      debug('controller/task.js: getting all tasks');
+      tasks.getAll(Task.INBOX);
     }
-    tasks.get(params.id)
-      .then(function(data) {
-        goToCorrectType(data)
-        $scope.task = data
-        $scope.unit = data.taskType === Task.MIT ? Task.ONE_DAY : Task.ONE_WEEK
-        lastType = $scope.task.taskType
-        $scope[Task.types[lastType]] = true
+    tasks.get(params.id).then(function(data) {
+      goToCorrectType(data);
+      $scope.task = data;
+      $scope.unit = data.taskType === Task.MIT ? Task.ONE_DAY : Task.ONE_WEEK;
+      lastType = $scope.task.taskType;
+      $scope[Task.types[lastType]] = true;
 
-        debug ('controllers/task.js: contexts ' + JSON.stringify(data.contexts));
-        if (data.contexts && data.contexts.length > 0) {
-            angular.forEach(data.contexts, function(context) {
-                $scope.contextsActive[context] = { active: true};
-                debug('$scope.contextsActive now ' + JSON.stringify($scope.contextsActive));
-            });
-        }
-        debug ('controllers/task.js: projects ' + JSON.stringify(data.projects));
-        if (data.projects && data.projects.length > 0) {
-            angular.forEach(data.projects, function(project) {
-                $scope.projectsActive[project] = { active: true};
-                debug('$scope.projectsActive now ' + JSON.stringify($scope.projectsActive));
-            });
-        }
-      })
+      debug ('controllers/task.js: contexts ' + JSON.stringify(data.contexts));
+      if (data.contexts && data.contexts.length > 0) {
+        angular.forEach(data.contexts, function(context) {
+          $scope.contextsActive[context] = { active: true};
+          debug('$scope.contextsActive now ' + JSON.stringify($scope.contextsActive));
+        });
+      }
+      debug ('controllers/task.js: projects ' + JSON.stringify(data.projects));
+      if (data.projects && data.projects.length > 0) {
+        angular.forEach(data.projects, function(project) {
+          $scope.projectsActive[project] = { active: true};
+          debug('$scope.projectsActive now ' + JSON.stringify($scope.projectsActive));
+        });
+      }
+    });
 
-    tasks.extend($scope)
+    tasks.extend($scope);
 
     function goToCollection() {
-      var navbarCtrl = angular.element('bp-navbar').controller('bpNavbar')
-      $state.go(navbarCtrl.getUpFromState(current).state)
+      var navbarCtrl = angular.element('bp-navbar').controller('bpNavbar');
+      $state.go(navbarCtrl.getUpFromState(current).state);
     }
 
     function goToCorrectType(task) {
       if (current.data.taskType !== task.taskType) {
-        $state.go(Task.types[task.taskType] + 'Task', {id: task.id})
+        $state.go(Task.types[task.taskType] + 'Task', {id: task.id});
       }
     }
 
     $scope.handle = function(promise) {
       promise.then(function(task) {
         if (task.taskType === Task.ARCHIVE || task.done) {
-          return goToCollection()
+          return goToCollection();
         }
-        goToCorrectType(task)
-      })
+        goToCorrectType(task);
+      });
     }
 
     $scope.update = function() {
-      return hoodie.store.update('task', $scope.task.id, $scope.task)
+      return hoodie.store.update('task', $scope.task.id, $scope.task);
     }
-  })
+  }
+);

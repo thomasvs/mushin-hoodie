775b1fbd46b657841dd6a6e790b2f8fbd9839cd3
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2014-10-21 14:52:00 -0700

    up indent

--- app/scripts/controllers/progress.js
+++ app/scripts/controllers/progress.js
@@ -17,40 +17,40 @@ angular.module('mushin').controller(
   'ProgressCtrl',
   function ($scope, $q, hoodie) {
 
-  $scope.instructions = true;
+    $scope.instructions = true;
 
-  /* state is inbox, lists, ... - the section for which instructions can
-   * be seen */
+    /* state is inbox, lists, ... - the section for which instructions can
+     * be seen */
 
-    /**
-     * @ngdoc    method
-     * @name     mushin.controller:ProgressCtrl#sawInstructions
-     * @methodOf mushin.controller:ProgressCtrl
-     * @param    {string} state the name of the section instructions have been
-     *                          seen for
-     *
-     * @description
-     *
-     * Mark the instructions for the given section as seen.
-     */
-  $scope.sawInstructions = function(state) {
-    var progress;
+      /**
+       * @ngdoc    method
+       * @name     mushin.controller:ProgressCtrl#sawInstructions
+       * @methodOf mushin.controller:ProgressCtrl
+       * @param    {string} state the name of the section instructions have been
+       *                          seen for
+       *
+       * @description
+       *
+       * Mark the instructions for the given section as seen.
+       */
+    $scope.sawInstructions = function(state) {
+      var progress;
 
-    $q.when(hoodie.store.find('progress', state))
-      .then(function(data) {
-        $scope.instructions = data.count > 1 ? false : true;
-        progress = data;
-      }, function() {
-        $scope.instructions = true
-        progress = {
-          id: state,
-          count: 0
-        };
-      })
-      .then(function() {
-        if (++progress.count <= 2) {
-          hoodie.store.add('progress', progress);
-        }
-      })
-  }
+      $q.when(hoodie.store.find('progress', state))
+        .then(function(data) {
+          $scope.instructions = data.count > 1 ? false : true;
+          progress = data;
+        }, function() {
+          $scope.instructions = true
+          progress = {
+            id: state,
+            count: 0
+          };
+        })
+        .then(function() {
+          if (++progress.count <= 2) {
+            hoodie.store.add('progress', progress);
+          }
+        })
+    }
 })

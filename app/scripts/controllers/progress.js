d483dec5301139ec8b00ba2fb5bcd80270d45f5f
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2014-10-21 15:04:09 -0700

    add debug and comments

--- app/scripts/controllers/progress.js
+++ app/scripts/controllers/progress.js
@@ -19,36 +19,46 @@ angular.module('mushin').controller(
 
     $scope.instructions = true;
 
-    /* state is inbox, lists, ... - the section for which instructions can
-     * be seen */
-
       /**
        * @ngdoc    method
        * @name     mushin.controller:ProgressCtrl#sawInstructions
        * @methodOf mushin.controller:ProgressCtrl
        * @param    {string} state the name of the section instructions have been
-       *                          seen for
+       *                          seen for (inbox, lists, ...)
        *
        * @description
        *
        * Mark the instructions for the given section as seen.
        */
     $scope.sawInstructions = function(state) {
+
+      var debug = new window.$debug('mushin:controllers/progress');
       var progress;
 
+      /* progress is stored in progress/(state) documents with id/count */
       $q.when(hoodie.store.find('progress', state))
         .then(function(data) {
+          debug('ProgressCtrl for state ' + state +
+            ' is ' + JSON.stringify(data));
           $scope.instructions = data.count > 1 ? false : true;
           progress = data;
         }, function() {
-          $scope.instructions = true
+          debug('No progress for state ' + state + ' in database');
+          $scope.instructions = true;
           progress = {
             id: state,
             count: 0
           };
         })
         .then(function() {
+          /* FIXME: for some reason zentodone decided to show the
+           *        instructions exactly twice
+           *        It gets created in-memory with 0, then incremented
+           *        to 1 and saved to database, then incremented to 2 next
+           *        time, from which point it no longer shows.
+           */
           if (++progress.count <= 2) {
+            debug('Adding progress for state ' + state + ' to database');
             hoodie.store.add('progress', progress);
           }
         })

00bc3aa763ea9da7e08076fac7699987904b63fc
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2014-10-21 14:48:29 -0700

    document and add semicolons;

--- app/scripts/controllers/progress.js
+++ app/scripts/controllers/progress.js
@@ -1,23 +1,55 @@
-angular.module('mushin').controller('ProgressCtrl', function ($scope, $q, hoodie) {
+// vi:si:et:sw=2:sts=2:ts=2
+/**
+ * @ngdoc     controller
+ * @name      mushin.controller:ProgressCtrl
+ * @requires  $scope
+ * @requires  $q
+ * @requires  hoodie
+ *
+ * @description <p>The progress controller
+ *                 keeps track of whether a user has seen the
+ *                 instructions for a particular view.
+ *              </p>
+ *
+ * @author    Thomas Vander Stichele <thomas (at) apestaart (dot) org>
+ */
+angular.module('mushin').controller(
+  'ProgressCtrl',
+  function ($scope, $q, hoodie) {
 
-  $scope.instructions = true
+  $scope.instructions = true;
 
+  /* state is inbox, lists, ... - the section for which instructions can
+   * be seen */
+
+    /**
+     * @ngdoc    method
+     * @name     mushin.controller:ProgressCtrl#sawInstructions
+     * @methodOf mushin.controller:ProgressCtrl
+     * @param    {string} state the name of the section instructions have been
+     *                          seen for
+     *
+     * @description
+     *
+     * Mark the instructions for the given section as seen.
+     */
   $scope.sawInstructions = function(state) {
-    var progress
+    var progress;
+
     $q.when(hoodie.store.find('progress', state))
       .then(function(data) {
-        $scope.instructions = data.count > 1 ? false : true
-        progress = data
+        $scope.instructions = data.count > 1 ? false : true;
+        progress = data;
       }, function() {
         $scope.instructions = true
         progress = {
           id: state,
           count: 0
-        }
+        };
       })
       .then(function() {
         if (++progress.count <= 2) {
-          hoodie.store.add('progress', progress)
+          hoodie.store.add('progress', progress);
         }
       })
   }

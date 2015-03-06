2b47a23e12706cde7190b42c117a3f5ae04afcb2
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2015-03-05 23:15:05 -0500

    add a real test that tests toggling, emit, and result

--- test/spec/controllers/numberlist.js
+++ test/spec/controllers/numberlist.js
@@ -1,6 +1,9 @@
 // vi:si:et:sw=2:sts=2:ts=2
 describe(
   'Controller: NumberListController', function () {
+
+    var scope, controller, createController;
+
     beforeEach(module('mushin'));
 
     beforeEach(
@@ -9,24 +12,47 @@ describe(
           scope = $rootScope.$new();
           controller = $controller;
 
+          createController = function(all, type, data, active, multiple) {
+            if (typeof all === 'undefined') { all = false; }
+            if (typeof type === 'undefined') { type = 'unknown-type'; }
+            if (typeof data === 'undefined') { data = 'data'; }
+            if (typeof active === 'undefined') { active = 'active'; }
+            if (typeof multiple === 'undefined') { multiple = true; }
+            attrs = {
+              'all':  all,
+              'type': type,
+              'data': data,
+              'active': active,
+              'multiple': multiple,
+            };
+
+            return $controller('NumberListController', {
+              $scope: scope,
+              $attrs: attrs,
+            });
+
+          };
+
         }
       )
     );
 
     it('should initialize even with boolean properties', function () {
-      attrs = {
-        'all':  false,
-        'type': 'unknown-type',
-        'data': 'haha',
-        'active': true,
-        'multiple': true,
-      };
+      var $nlc = createController(false, 'unknown-type', 'haha', true,
+        true);
+    });
 
-      var $nlc = controller('NumberListController', {
-        $scope: scope,
-        $attrs: attrs,
-      });
+    it('should emit NUMBER_TOGGLED with type and name', function () {
+      scope.active = {};
+      var $nlc = createController();
 
+      spyOn(scope, "$emit");
+
+      scope.toggle('1');
+
+      expect(scope.$emit).toHaveBeenCalledWith(
+        "NUMBER_TOGGLED", "unknown-type", "1");
+      expect(scope.active["1"]).toEqual({ "active": true });
     });
   }
 );

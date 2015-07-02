f09f1aca175beb872c5e43a1eade68d35a1e4e0b
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2015-07-02 15:59:47 -0400

    Add a test.
    Increased coverage; now
    
    Statements   : 24.69% ( 259/1049 )
    Branches     : 8.99% ( 31/345 )
    Functions    : 13.92% ( 22/158 )
    Lines        : 24.93% ( 258/1035 )

---
+++ test/spec/services/thing.js
@@ -0,0 +1,33 @@
+// vi:si:et:sw=2:sts=2:ts=2
+
+/**
+ * @fileoverview tests for scripts/services/things.js
+ */
+
+describe(
+  'Service: Thing', function () {
+    var Thing;
+
+    var debug = new window.$debug('mushin:Thing');
+    debug('TEST: Service: Thing debug');
+
+    // load our app so injecting works
+    beforeEach(module('mushin'));
+
+    // we're only testing Thing, so no need to inject more
+    beforeEach(inject(function (_Thing_) {
+      Thing = _Thing_;
+    }));
+
+    // tests
+
+    it('should instantiate new things',
+      function () {
+        thing = new Thing('my thing', 'is good', {
+        });
+
+        expect(thing.data.state).toEqual(Thing.ACTIVE);
+      }
+    );
+  }
+);
